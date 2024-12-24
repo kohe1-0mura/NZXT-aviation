c70335a64398a433aa1b8b2f71c7624a78665939
大村耕平 <omura@oomurakouheinoMacBook-Pro.local>
2024-12-24 18:26:23 +0900

    commit

--- src/components/KrakenDisplay.jsx
+++ src/components/KrakenDisplay.jsx
@@ -34,7 +34,7 @@ const RadialGaugeContainer = styled.div`
   flex-direction: row;
   justify-content: space-between;
   height: 247px;
-  padding: 0 30px 0 40px;
+  padding: 0 30px 0 50px;
 `;
 
 const CenterContainer = styled.div`
